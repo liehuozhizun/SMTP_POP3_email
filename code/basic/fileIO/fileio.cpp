5b8557e08f94193d6f3093137e7d356fedb670e5
Hang Yuan <hyuan3@ucsc.edu>
2019-08-10 18:32:25 -0700

    Basic: FileIO: finish header file for FileIO

--- code/basic/fileIO/fileio.cpp
+++ code/basic/fileIO/fileio.cpp
@@ -8,6 +8,28 @@
  *
  */
 
-typedef int RC;
-
 #include "fileio.h"
+
+FileIO* FileIO::_file_io = NULL;
+
+FileIO* FileIO::instance()
+{
+    if(!_file_io)
+        _file_io = new FileIO();
+
+    return _file_io;
+}
+
+
+RC FileIO::CreateFile  (const string &fileName)
+RC FileIO::CreateDir   (const string &dirName)
+RC FileIO::DestroyFile (const string &fileName)
+RC FileIO::DestroyDir  (const string &dirName)
+RC FileIO::OpenFile    (const string &fileName)
+RC FileIO::CloseFile   (const string &fileName)
+RC FileIO::ResetFile   (const string &fileName)
+RC FileIO::ResetDir    (const string &dirName)
+RC FileIO::WriteFile   (const string &fileName, size_t offset, const void *data)
+RC FileIO::ReadFile    (const string &fileName, size_t offset, size_t length, void *data)
+RC FileIO::AppendFile  (const string &fileName, size_t length, const void *data)
+RC FileIO::FileExists  (const string &fileName)
--- code/basic/fileIO/fileio.h
+++ code/basic/fileIO/fileio.h
@@ -1,11 +1,10 @@
 #ifndef FILE_IO
 #define FILE_IO
 
-#define SUCCESS 0
-
 #include <cstio>
 #include <string>
 #include <climits>
+#include <filesystem>
 using namespace std;
 
 /**
@@ -13,12 +12,17 @@ using namespace std;
  * This class contains all interfaces that will be used to manage the file.
  *
  * Contained Public Functions:
- *   RC Init        ()
- *   RC ReInit      ()
  *   RC CreateFile  (const string &fileName)
+ *   RC CreateDir   (const string &dirName)
  *   RC DestroyFile (const string &fileName)
+ *   RC DestroyDir  (const string &dirName)
  *   RC OpenFile    (const string &fileName)
  *   RC CloseFile   (const string &fileName)
+ *   RC ResetFile   (const string &fileName)
+ *   RC ResetDir    (const string &dirName)
+ *   RC WriteFile   (const string &fileName, size_t offset, const void *data)
+ *   RC ReadFile    (const string &fileName, size_t offset, size_t length, void *data)
+ *   RC AppendFile  (const string &fileName, size_t length, const void *data)
  */
 
 class FileIO
@@ -36,45 +40,119 @@ public:
   static FileIO* instance();
 
   /**
-   * This function check the current files stored in data directory to see if all
-   * the required fundemental functions exists and are available to read/write.
-   * If any file are missing, this function will create the missing one. If any
-   * file is unaccessible, this function will throw and error to the caller.
-   * @param  none
-   * @return SUCCESS if every fundemental file exists and accessible
-   *         STANDARD_ERROR if any fundemental file is lost or unaccessible
-   */ 
-  RC Init ();
-
-  /**
-   * These function transitions between rooms. Each call should return SUCCESS if the current room has
-   * an exit in the correct direction and the new room was able to be loaded, and STANDARD_ERROR
-   * otherwise.
-   * @return SUCCESS if the room CAN be navigated to and changing the current room to that new room
-   *         succeeded.
+   * This function will create a new empty file with the given filename.
+   * @param  const string given as filename
+   * @return SUCCESS if the file is successfully created
+   *         CREATE_ERROR if create file not successfully
    */
   RC CreateFile  (const string &fileName);
 
   /**
-   * These function transitions between rooms. Each call should return SUCCESS if the current room has
-   * an exit in the correct direction and the new room was able to be loaded, and STANDARD_ERROR
-   * otherwise.
-   * @return SUCCESS if the room CAN be navigated to and changing the current room to that new room
-   *         succeeded.
+   * This function will create a new directory in the data folder.
+   * @param  const string given as directory name
+   * @return SUCCESS if the directory is successfully created
+   *         CREATE_ERROR if create directory not successfully
+   */
+  RC CreateDir   (const string &dirname);
+
+  /**
+   * This function will remove a file with the given filename.
+   * @param  const string given as filename
+   * @return SUCCESS if the file has been removed successfully
+   *         DESTROY_ERROR if failed to remove
    */
   RC DestroyFile (const string &fileName);
+
+  /**
+   * This function will remove a directory with the given directory name.
+   * @param  const string given as directory name
+   * @return SUCCESS if the directory has been removed successfully
+   *         DESTROY_ERROR if failed to remove
+   */
+  RC DestroyFile (const string &fileName);
+
+  /**
+   * This function will open a file with the given filename or path to open the
+   * file wit read and write accesses and set the file descriptor appropriatly.
+   * @param  const string given as the filename
+   * @return SUCCESS if the file is successfully opened
+   *         OPEN_ERROR if file cannot be opened
+   */
   RC OpenFile    (const string &fileName);
+
+  /**
+   * This function will close the file descriptor _fd
+   * @param  const string given as the filename
+   * @return SUCCESS if the _fd is successfully closed
+   *         OPEN_ERROR if _fd cannot be closed
+   */
   RC CloseFile   (const string &fileName);
+
+  /**
+   * This function will reset the file that already exists with the given filename.
+   * This function will first destory that file and then re-create it.
+   * @param  const string given as fileName
+   * @return SUCCESS if destory and create successfully
+   *         pre-defined error number returned by other functions
+   */
+  RC ResetFile   (const string &fileName);
+
+  /**
+   * This function will reset the directory that already exists with the given name.
+   * This function will first destory that directory and then re-create it.
+   * @param  const string given as dirctory name
+   * @return SUCCESS if destory and create successfully
+   *         pre-defined error number returned by other functions
+   */
+  RC ResetDir    (const string &dirName);
+
+  /**
+   * This function will append(write) the fixed-length data from the given pointer
+   * at the specific position in the specified file
+   * @param  const string given as filename (can be a path)
+   *         size_t offset indicates the starting position in the file
+   *         size_t length indicates how long need to be read
+   *         void * indicates the pointer that need to store the data
+   * @return SUCCESS if write successfully
+   *         WRITE_ERROR or other pre-defined error if failed to write
+   */
+  RC WriteFile  (const string &fileName, size_t offset, size_t length, const void *data);
+
+  /**
+   * This function will read the fixed-length data from speicified file and copy
+   * that data to given char pointer
+   * @param  const string given as filename
+   *         size_t offset indicates the starting position in the file
+   *         size_t length indicates how long need to be read
+   *         void * indicates the pointer that need to store the data
+   * @return SUCCESS if read successfully
+   *         READ_ERROR or other pre-defined error if failed to read
+   */
+  RC ReadFile    (const string &fileName, size_t offset, size_t length, void *data);
+
+  /**
+   * This function will append(write) the fixed-length data from the given pointer
+   * at the end of the speicified file
+   * @param  const string given as filename (can be a path)
+   *         size_t length indicates how long need to be append
+   *         void * indicates the pointer that stores the data need to be stored
+   * @return SUCCESS if append successfully
+   *         APPEND_ERROR or other pre-defined error if failed to append
+   */
+  RC AppendFile  (const string &fileName, size_t length, const void *data);
 
 protected:
   FileIO() {};   // Constructor
   ~FileIO() {};  // Destructor
 
 private:
-  FILE *_fd;                              // File descriptor, from <cstio>
+  FILE *_fd;                               // File descriptor, from <cstio>
+  static FileIO *_file_io;    // Pointer of this class
 
-  void _setfd(FILE *fd) { _fd = fd;   };  // Set the current file descriptor
-  FILE *_getfd()        { return _fd; };  // Get current file descriptor
+  // Private helper function
+  void Setfd(FILE *fd) { _fd = fd;   };    // Set the current file descriptor
+  FILE *Getfd()        { return _fd; };    // Get current file descriptor
+  bool fileExists(const string &fileName); // Check if the file exists
 }
 
 #endif
